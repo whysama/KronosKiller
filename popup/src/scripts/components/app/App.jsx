b4debd6c82fb0a9324404edcded0dc4afee067f5
Shenyang Zhou <zhou.shenyang.young@gmail.com>
2017-03-29 00:03:04 +0200

    Test react-day-picker

--- popup/src/scripts/components/app/App.jsx
+++ popup/src/scripts/components/app/App.jsx
@@ -1,20 +1,27 @@
 import React, {Component} from 'react';
+import DayPicker from '../day-picker/DayPicker';
 //import {connect} from 'react-redux';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects : this.props.projects
+    }
+  }
   
   handleClick(){
-    console.log(this.props.projects);
+    console.log(this.state.projects);
   }
 
   render() {
     return (
+      <div>
       <button className="app" onClick={() => this.handleClick()}>
         test
       </button>
+        <DayPicker/>
+      </div>
     );
   }
 }
