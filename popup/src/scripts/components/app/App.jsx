028a78bf53fae006ebec52a9d21f817a199916a6
Shenyang Zhou <zhou.shenyang.young@gmail.com>
2017-04-04 13:32:16 +0200

    React Move DayPicker state To App state

--- popup/src/scripts/components/app/App.jsx
+++ popup/src/scripts/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import DayPicker from '../day-picker/DayPicker';
+import { DateUtils } from 'react-day-picker';
 //import {connect} from 'react-redux';
 import '../../../css/style.css';
 
@@ -9,12 +10,27 @@ class App extends Component {
     this.state = {
       projects : this.props.projects,
       period : this.props.period,
-      disabled : this.props.disabled
+      disabled : this.props.disabled,
+      selectedDays : []
     }
+    this.handleDayClick = this.handleDayClick.bind(this);
   }
   
   handleClick(){
     console.log(this.state.projects);
+  }
+
+  handleDayClick(day, { selected }){
+    const { selectedDays } = this.state;
+        if (selected) {
+            const selectedIndex = selectedDays.findIndex(selectedDay =>
+                DateUtils.isSameDay(selectedDay, day),
+            );
+            selectedDays.splice(selectedIndex, 1);
+        } else {
+            selectedDays.push(day);
+        }
+        this.setState({ selectedDays });
   }
 
   render() {
@@ -27,6 +43,8 @@ class App extends Component {
                 <DayPicker
                     initialMonth={ new Date(this.props.period.year,this.props.period.month) } 
                     disabled={this.state.disabled}
+                    selectedDays = { this.state.selectedDays }
+                    onDayClick = { this.handleDayClick }
                     />
             </div>
         </div>
--- popup/src/scripts/components/day-picker/DayPicker.js
+++ popup/src/scripts/components/day-picker/DayPicker.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import moment from 'moment';
-import DayPicker, { DateUtils } from 'react-day-picker';
+import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
 export default class SelectMultipleDays extends React.Component {
     constructor(props) {
         super(props);
-        this.handleDayClick = this.handleDayClick.bind(this);
         this.disabledDays = this.disabledDays.bind(this);
         this.state = {
             selectedDays: [],
         };
-    }
-
-    handleDayClick(day, { selected }) {
-        const { selectedDays } = this.state;
-        if (selected) {
-            const selectedIndex = selectedDays.findIndex(selectedDay =>
-                DateUtils.isSameDay(selectedDay, day),
-            );
-            selectedDays.splice(selectedIndex, 1);
-        } else {
-            selectedDays.push(day);
-        }
-        this.setState({ selectedDays });
     }
 
     disabledDays(day){
@@ -37,8 +23,8 @@ export default class SelectMultipleDays extends React.Component {
 	            	canChangeMonth = { false }
 	            	initialMonth={ this.props.initialMonth }
 	            	disabledDays={ this.disabledDays }
-		            selectedDays = { this.state.selectedDays }
-	            	onDayClick = { this.handleDayClick }
+		            selectedDays = { this.props.selectedDays }
+	            	onDayClick = { this.props.onDayClick }
 	            /> 
 	        </div>
         );
